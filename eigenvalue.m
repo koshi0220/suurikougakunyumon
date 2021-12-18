b9917b6ede4c6ba25ec134d8130462545a057caa
koshi <koshi1971@outlook.jp>
2021-12-18 20:03:11 +0900

    added figures

--- eigenvalue.m
+++ eigenvalue.m
@@ -10,8 +10,11 @@ x0 = nthroot((-q + sqrt(q^2 + 4*p^3))/2,3) + nthroot((-q - sqrt(q^2 + 4*p^3))/2,
 y0 = (a - x0)/b;
 
 syms x y
+syms u(x,y) v(x,y)
+u(x,y) = c*(y + x - (x^3)/3 + z);
+v(x,y) = -(x - a + b*y)/c;
 
-A(x,y) = jacobian([c*(y + x - (x^3)/3 + z),-(x - a + b*y)/c],[x,y]);
+A(x,y) = jacobian([u,v],[x,y]);
 A1 = A(x0,y0);
 
 lambda = eig(A1);
